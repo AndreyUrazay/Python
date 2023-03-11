e8a702a5bcb92912879f482b0e93c36ebb90a3c2
AndreyUrazay <big_di92@mail.ru>
2023-03-11 23:34:50 +0700

    add Seminar_006

--- Seminar_006/Simple_043.py
+++ Seminar_006/Simple_043.py
@@ -8,6 +8,11 @@
 # ввод 1 2 3 2 3
 # вывод 2
 
+
+# variant 1
+print('-------------------')
+print('Variant 1')
+
 lst = []
 n = int(input('Введите длину списка: '))
 for i in range(n):
@@ -20,3 +25,31 @@ for i in range(n - 1):
         if lst[i] == lst[j]:
             count += 1
 print(count)
+
+
+# variant 2
+print('-------------------')
+print('Variant 2')
+from random import randint
+# a = [randint(0, 5) for i in range(int(input('Количество элементов массива: ')))]
+a = [5, 5, 5, 5, 5, 4, 4, 4, 4, 4]
+cnt = 0 
+for j in range(len(a)):
+    cnt += a[j + 1:].count(a[j])
+print(a, cnt)
+
+
+# variant 3
+print('-------------------')
+print('Variant 3')
+
+def func(lst: list) -> int:
+    el, *lst = lst # el = lst[0], lst = [1:]
+    if lst:
+        return func(lst) + lst.count(el)
+
+    return 0
+
+
+if __name__ == '__main__':
+    print(func([1, 2, 3, 2, 3]))
