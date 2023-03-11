ef3000bddd9db836a2c48046aad60089164917d7
AndreyUrazay <big_di92@mail.ru>
2023-03-11 20:30:20 +0700

    add Simple_031,_033,_035,_037

--- Seminar_005/Simple_031.py
+++ Seminar_005/Simple_031.py
@@ -7,13 +7,20 @@
 
 # Задание необходимо решать через рекурсию
 
-#считаем через индекс
+# считаем через индекс
 
-
+# variant 1
 def fibo(n):
 
     if n in [0, 1]:
         return n
     return fibo(n-1)+fibo(n-2)
 
+
 print(fibo(int(input())))
+
+
+# variant 2
+def fib(N): return N if N in (0, 1) else fib(N-1) + fib(N-2)
+
+print(fib(7))
