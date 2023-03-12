7c19fef2c50d49af44e48bfff0f4341963628bc3
AndreyUrazay <big_di92@mail.ru>
2023-03-12 19:35:03 +0700

    add Lection

--- Lection/Lection_004/work_with_OS.py
+++ Lection/Lection_004/work_with_OS.py
@@ -1,9 +1,9 @@
-os.chdir(path) - смена текущей директории
+# os.chdir(path) - смена текущей директории
 
-os.getcwd() - текущая рабочая директория
+# os.getcwd() - текущая рабочая директория
 
-os.path.basename(path) - базовое имя пути
+# os.path.basename(path) - базовое имя пути
 
-os.path.abspath(path) - возвращает нормализованный абсолютный путь
+# os.path.abspath(path) - возвращает нормализованный абсолютный путь
 
-shutil 
+# shutil 
